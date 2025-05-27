c1bf254c693dfeaffe7f377ba26254055d33200f
/RX7900XTX/ <168472716+Cat-Gawr@users.noreply.github.com>
2025-05-27 17:35:58 +0200

    Create MicrophoneComunicable.java

---
+++ app/src/main/java/niie/translator/YTRANAS/tools/gui/MicrophoneComunicable.java
@@ -0,0 +1,7 @@
+
+package nie.translator.rtranslator.tools.gui;
+
+public interface MicrophoneComunicable {
+    void startMicrophone(boolean changeAspect);
+    void stopMicrophone(boolean changeAspect);
+}
