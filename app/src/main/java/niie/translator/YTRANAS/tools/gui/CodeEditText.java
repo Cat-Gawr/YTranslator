97af3c97afe52b560dfc9fc292d83a895e6fcade
/RX7900XTX/ <168472716+Cat-Gawr@users.noreply.github.com>
2025-05-27 17:39:52 +0200

    Create CodeEditText.java

---
+++ app/src/main/java/niie/translator/YTRANAS/tools/gui/CodeEditText.java
@@ -0,0 +1,20 @@
+
+package nie.translator.rtranslator.tools.gui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+public class CodeEditText extends androidx.appcompat.widget.AppCompatEditText {
+
+    public CodeEditText(Context context) {
+        super(context);
+    }
+
+    public CodeEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CodeEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
