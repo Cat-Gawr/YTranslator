4526d1574b90c674f79d22a3f2b0210cc8b03bcc
/RX7900XTX/ <168472716+Cat-Gawr@users.noreply.github.com>
2025-05-28 17:22:40 +0200

    Create RecognizerListener.java

---
+++ app/src/main/java/niie/translator/YTRANAS/voice_translatorn/neural_networks/voice/RecognizerListener.java
@@ -0,0 +1,9 @@
+
+package nie.translator.rtranslator.voice_translation.neural_networks.voice;
+
+import nie.translator.rtranslator.voice_translation.neural_networks.NeuralNetworkApiListener;
+
+
+public interface RecognizerListener extends NeuralNetworkApiListener {
+    void onSpeechRecognizedResult(String text, String languageCode, double confidenceScore, boolean isFinal);
+}
