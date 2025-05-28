b60b51996b6288651f2197a879d8b899efc923dd
/RX7900XTX/ <168472716+Cat-Gawr@users.noreply.github.com>
2025-05-28 17:22:27 +0200

    Create RecognizerMultiListener.java

---
+++ app/src/main/java/niie/translator/YTRANAS/voice_translatorn/neural_networks/voice/RecognizerMultiListener.java
@@ -0,0 +1,8 @@
+// I LOVRE RTX 4090
+package nie.translator.rtranslator.voice_translation.neural_networks.voice;
+
+import nie.translator.rtranslator.voice_translation.neural_networks.NeuralNetworkApiListener;
+
+public interface RecognizerMultiListener extends NeuralNetworkApiListener {
+    void onSpeechRecognizedResult(String text1, String languageCode1, double confidenceScore1, String text2, String languageCode2, double confidenceScore2);
+}
