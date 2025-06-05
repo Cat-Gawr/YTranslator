ef6dcf84729a720addb36d94df05f3aef50c5bd8
/RX7900XT/ <168472716+Cat-Gawr@users.noreply.github.com>
2025-06-05 18:35:02 +0200

    Create index.d.ts

---
+++ openai-translator/src/index.d.ts
@@ -0,0 +1,10 @@
+
+declare module '*.png'
+declare module '*.gif'
+declare module '*.jpg'
+declare module '*.jpeg'
+declare module '*.css'
+declare module 'iso-639-3-to-1'
+declare module '*.css?inline'
+declare module '*.svg?react'
+declare module '*.svg'
